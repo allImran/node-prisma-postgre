a13bd86f0f0aca723348627ee9f29038e330991b
allImran <alemrandev@gmail.com>
2023-06-27 00:00:40 +0600

    add route

---
+++ src/router.js
@@ -0,0 +1,32 @@
+import { Router } from 'express'
+
+const router = new Router()
+
+/**
+ * product
+ */
+router.get('/product', () =>{})
+router.get('/product/:id', () =>{})
+router.put('/product/:id', () =>{})
+router.post('/product/', () =>{})
+router.delete('/product/:id', () =>{})
+
+/**
+ * update
+ */
+router.get('/update', () =>{})
+router.get('/update/:id', () =>{})
+router.put('/update/:id', () =>{})
+router.post('/update/', () =>{})
+router.delete('/update/:id', () =>{})
+
+/**
+ * update Point
+ */
+router.get('/updatepoint', () =>{})
+router.get('/updatepoint/:id', () =>{})
+router.put('/updatepoint/:id', () =>{})
+router.post('/updatepoint/', () =>{})
+router.delete('/updatepoint/:id', () =>{})
+
+export default router

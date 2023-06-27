6a8c4ca527aff6ef08d2804f371bbf9445ca2134
allImran <alemrandev@gmail.com>
2023-06-27 21:55:11 +0600

    setup route

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import router from './router'
+
+const app = express()
+
+// app.get('/', (req, res) => {
+//     console.log('Hello from express')
+//     res.status(200)
+//     res.json({ message: 'Hello from express' })
+// })
+
+
+app.use('/api', router)
+
+export default app

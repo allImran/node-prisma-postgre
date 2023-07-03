cf1f39f77c90d7b26b5cb0c6d3798d9afd9b2719
allImran <alemrandev@gmail.com>
2023-07-03 22:27:09 +0600

    add jwt

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import router from './router'
 import morgan from 'morgan'
 import cors from 'cors'
+import { protect } from './modules/auth'
 
 const app = express()
 
@@ -24,6 +25,6 @@ app.use(custom('custom'))
 // })
 
 
-app.use('/api', router)
+app.use('/api', protect, router)
 
 export default app
